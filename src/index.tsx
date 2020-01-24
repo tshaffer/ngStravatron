1c39fe4b4397a611c8ef5a4356b0094d08e67add
Ted Shaffer <ted@brightsign.biz>
2020-01-24 07:48:24 -0800

    evolution, part 1

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,25 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { Provider } from "react-redux";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
-import { App } from "./App";
-import store from "./store/store";
+import { createStore, applyMiddleware, compose } from 'redux';
+
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+
+import rootReducer from './reducers';
+
+import { App } from './App';
+// import store from './store/store';
+
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer, /* preloadedState, */ composeEnhancers(
+    applyMiddleware(thunkMiddleware)
+  ));
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
