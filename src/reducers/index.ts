d345bd5f41904f862c0086835d695df9c6b5a0c4
Ted Shaffer <ted@brightsign.biz>
2020-01-25 07:18:44 -0800

    remove unused boilerplate code

--- src/reducers/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { todoReducer } from "./todoReducers";
-
-const rootReducer = combineReducers({
-  todo: todoReducer
-});
-
-export default rootReducer;
