cb37d6a2213a44ac8cc88aad82bc6f404e277ff2
Ted Shaffer <ted@brightsign.biz>
2020-01-26 07:11:28 -0800

    lots of code copied in; I don't understand it all; I'm going to simplify it so that I understand it.

--- src/components/activities.tsx
+++ src/components/activities.tsx
@@ -12,11 +12,89 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
 import Button from '@material-ui/core/Button';
 
 import { ActivitiesMap, StravatronSummaryActivity, StravatronActivity } from '../types';
 import { getActivities } from '../selectors';
 import * as Converters from '../utilities/converters';
+
+function getSorting<K extends keyof any>(
+  order: Order,
+  orderBy: K,
+): (a: { [key in K]: number | string }, b: { [key in K]: number | string }) => number {
+  return order === 'desc' ? (a, b) => desc(a, b, orderBy) : (a, b) => -desc(a, b, orderBy);
+}
+
+type Order = 'asc' | 'desc';
+interface ActivityData {
+  date: string;
+  name: string;
+  ridingTime: number;
+  distance: number;
+  elevation: number;
+  kilojoules: number;
+  np: number;
+  tss: number;
+  averageWatts: number;
+  maxWatts: number;
+  averageHeartrate: number;
+  maxHeartrate: number;
+}
+
+interface HeadCell {
+  disablePadding: boolean;
+  id: keyof ActivityData;
+  label: string;
+  numeric: boolean;
+}
+
+const headCells: HeadCell[] = [
+  { id: 'date', numeric: false, disablePadding: true, label: 'Date' },
+  { id: 'name', numeric: false, disablePadding: true, label: 'Name' },
+  { id: 'ridingTime', numeric: true, disablePadding: false, label: 'Riding Time' },
+  { id: 'distance', numeric: true, disablePadding: false, label: 'Distance' },
+  { id: 'elevation', numeric: true, disablePadding: false, label: 'Elevation' },
+  { id: 'kilojoules', numeric: true, disablePadding: false, label: 'Kilojoules' },
+  { id: 'np', numeric: true, disablePadding: false, label: 'NP' },
+  { id: 'tss', numeric: true, disablePadding: false, label: 'TSS' },
+  { id: 'averageWatts', numeric: true, disablePadding: false, label: 'Average Watts' },
+  { id: 'maxWatts', numeric: true, disablePadding: false, label: 'Max Watts' },
+  { id: 'averageHeartrate', numeric: true, disablePadding: false, label: 'Average Heartrate' },
+  { id: 'maxHeartrate', numeric: true, disablePadding: false, label: 'Max Heartrate' },
+];
+
+const handleRequestSort = (event: React.MouseEvent<unknown>, property: keyof ActivityData) => {
+  const isAsc = orderBy === property && order === 'asc';
+  setOrder(isAsc ? 'desc' : 'asc');
+  setOrderBy(property);
+};
+
+const createSortHandler = (property: keyof ActivityData) => (event: React.MouseEvent<unknown>) => {
+  onRequestSort(event, property);
+};
+
+function desc<T>(a: T, b: T, orderBy: keyof T) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function stableSort<T>(array: T[], cmp: (a: T, b: T) => number) {
+  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
+  stabilizedThis.sort((a, b) => {
+    const order = cmp(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map(el => el[0]);
+}
+
+
 
 export interface ActivitiesProps {
   activities: ActivitiesMap;
@@ -237,6 +315,131 @@ class Activities extends React.Component<ActivitiesProps> {
     return (
       <div>
         Loading...
+      </div>
+    );
+  }
+
+  /*
+          <TableCell style={{ width: '64px', whiteSpace: 'normal' }}>Date</TableCell>
+          <TableCell style={{ width: '192px' }}>Name</TableCell>
+          <TableCell style={{ width: '64px', whiteSpace: 'normal' }}>Riding Time</TableCell>
+          <TableCell style={{ width: '64px', whiteSpace: 'normal' }}>Distance</TableCell>
+          <TableCell style={{ width: '64px', whiteSpace: 'normal' }}>Elevation</TableCell>
+          <TableCell style={{ width: '64px', whiteSpace: 'normal' }}>Kilojoules</TableCell>
+          <TableCell style={{ width: '64px', whiteSpace: 'normal' }}>NP</TableCell>
+          <TableCell style={{ width: '64px', whiteSpace: 'normal' }}>TSS</TableCell>
+          <TableCell style={{ width: '64px', whiteSpace: 'normal' }}>Average Watts</TableCell>
+          <TableCell style={{ width: '64px', whiteSpace: 'normal' }}>Max Watts</TableCell>
+          <TableCell style={{ width: '64px', whiteSpace: 'normal' }}>Average Heartrate</TableCell>
+          <TableCell style={{ width: '64px', whiteSpace: 'normal' }}>Max Heartrate</TableCell>
+          <TableCell style={{ width: '64px', whiteSpace: 'normal' }}></TableCell>
+  */
+
+  getTableHead(): any {
+
+    const orderBy: string = 'date';
+    const order: Order = 'asc';
+
+    return (
+      <TableHead>
+        <TableRow>
+          {headCells.map((headCell) => (
+            <TableCell
+              key={headCell.id}
+              align={headCell.numeric ? 'right' : 'left'}
+              padding={headCell.disablePadding ? 'none' : 'default'}
+              sortDirection={orderBy === headCell.id ? order : false}
+            >
+              <TableSortLabel
+                active={orderBy === headCell.id}
+                direction={orderBy === headCell.id ? order : 'asc'}
+                onClick={createSortHandler(headCell.id)}
+              >
+                {headCell.label}
+                {orderBy === headCell.id ? (
+                  <span>
+                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                  </span>
+                ) : null}
+              </TableSortLabel>
+            </TableCell>
+          ))}
+        </TableRow>
+      </TableHead>
+    );
+  }
+
+  enhancedTable(): any {
+
+    const rows: ActivityData[] = [];
+    const activities: StravatronSummaryActivity[] = [];
+
+    const activitiesLUT = this.props.activities;
+
+    for (const activityId in activitiesLUT) {
+      if (activitiesLUT.hasOwnProperty(activityId)) {
+        const activity = activitiesLUT[activityId];
+
+        const row: ActivityData = {
+          date: activity.startDateLocal.toDateString(),
+          name: activity.name,
+          ridingTime: activity.elapsedTime,
+          distance: activity.distance,
+          elevation: activity.totalElevationGain,
+          kilojoules: activity.kilojoules,
+          np: 0,
+          tss: 0,
+          averageWatts: 0,
+          maxWatts: 0,
+          averageHeartrate: 0,
+          maxHeartrate: 0,
+        };
+        rows.push(row);
+      }
+    }
+
+    const [order, setOrder] = React.useState<Order>('desc');
+    const [orderBy, setOrderBy] = React.useState<keyof ActivityData>('date');
+
+    const tableHead = this.getTableHead();
+    return (
+      <div id='SummaryActivities'>
+        <br />
+        <TableContainer>
+          <Table
+            style={{ width: '1597px' }}
+            size={'medium'}
+          >
+            {tableHead}
+            <TableBody>
+            {stableSort(rows, getSorting(order, orderBy))
+                .map((row, index) => {
+                  return (
+                    <TableRow
+                      hover
+                      tabIndex={-1}
+                      key={row.name}
+                    >
+                      <TableCell component='th' scope='row' padding='none'>
+                        {row.date}
+                      </TableCell>
+                      <TableCell align='left'>{row.name}</TableCell>
+                      <TableCell align='right'>{row.ridingTime}</TableCell>
+                      <TableCell align='right'>{row.distance}</TableCell>
+                      <TableCell align='right'>{row.elevation}</TableCell>
+                      <TableCell align='right'>{row.kilojoules}</TableCell>
+                      <TableCell align='right'>{row.np}</TableCell>
+                      <TableCell align='right'>{row.tss}</TableCell>
+                      <TableCell align='right'>{row.averageWatts}</TableCell>
+                      <TableCell align='right'>{row.maxWatts}</TableCell>
+                      <TableCell align='right'>{row.averageHeartrate}</TableCell>
+                      <TableCell align='right'>{row.maxHeartrate}</TableCell>
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </div>
     );
   }
